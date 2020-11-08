166fc1348721140d6e70c6c12ed74f996de53cb1
Aditya2009 <70028124+Aditya2009@users.noreply.github.com>
2020-11-08 10:55:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("button")
+const circle = document.getElementById("circle")
+const square = document.getElementById("square")
+const rectangle = document.getElementById("rectangle")
+
+var numberInTheShape=1
+var oldValue=0
+
+button.onclick=()=>{
+    const $box=document.getElementById("box")
+    let n = document.getElementById("number").value
+
+    n = Number(n) + Number(oldValue);
+
+    for(let j=numberInTheShape; j<=n; j++) {
+        var shape = document.createElement("div")
+        shape.innerHTML += numberInTheShape;
+
+        if(square.checked) {
+            shape.classList.add("scquare")
+        }
+        if(circle.checked) {
+            shape.classList.add("circle")
+        }
+        box.appendChild(shape);
+        numberInTheShape++;
+        oldValue = box.lastElementChild.innerHTML;
+    }
+} 
